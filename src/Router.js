87a862c531b4b8e1cdcc943f99a826df4a88097f
moonseunghun <mangokkaksae2@gmail.com>
2023-05-30 14:13:29 +0900

    Add : kakao oauth

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Main from './Main/Main';
 import Footer from './Footer/Footer';
 import Nav from './Nav/Nav';
 import Detail from './Detial/Cart';
+import KakaoLogin from './KakaoLogin/KakaoLogin';
 // import Payment from './Payment/Payment';
 
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
       <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/oauth/kakao/callback" element={<KakaoLogin />} />
         <Route path="/" element={<Main />} />
         <Route path="/footer" element={<Footer />} />
         <Route path="/Detail" element={<Detail />} />
