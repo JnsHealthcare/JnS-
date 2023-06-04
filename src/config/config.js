06f12916745ef78ec048422068b7bb79e15585af
moonseunghun <mangokkaksae2@gmail.com>
2023-06-04 17:24:56 +0900

    Fix : url

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,6 @@ const BASE_URL = 'http://localhost:3000';
 const API = {
   login: `${BASE_URL}/login`,
   signup: `${BASE_URL}/auth/signUp`,
-  signin: `${BASE_URL}/auth/signIn`,
   detail: `${BASE_URL}/products/detail`,
   products: `${BASE_URL}/products/category`,
   cart: `${BASE_URL}/cart/`,
